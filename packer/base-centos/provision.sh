28b6f1b56bbc5308d4894fba0762ad6a107f7c0d
Duncan Dickinson <duncan@dickinson.name>
2015-11-11 15:28:48 +1000

    Trimmed down base-centos kickstart by shifting disk cleanup to a post script

--- packer/base-centos/provision.sh
+++ packer/base-centos/provision.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+# Apply the most current updates, add some extra packages and clean yum data
 sudo yum -y update
+sudo yum -y install openscap-utils scap-security-guide
+sudo yum -y clean all
+
+#Tidy up unused storage
+sudo swapoff -a
+sudo mkswap /dev/mapper/vg_vagrantcentos-lv_swap
+sudo dd if=/dev/zero of=/boot/EMPTY bs=1M
+sudo rm -f /boot/EMPTY
+sudo dd if=/dev/zero of=/EMPTY bs=1M
+sudo rm -f /EMPTY
